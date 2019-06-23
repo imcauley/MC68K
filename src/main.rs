4d66a3fcd6a30b7b622db0b0f6869d17398d3d33
Isaac McAuley <mcauleyisaac@gmail.com>
2019-06-23 14:30:56 -0600

    Added better support for size conversions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::process;
 
+const BYTE: u8 = 0;
+const WORD: u8 = 1;
+const LONG: u8 = 3;
+
 const DRD: u8 = 0;
 const ARD: u8 = 1;
 
@@ -37,12 +41,27 @@ fn get_source(mem:Memory, code:u16, eam:u8, place:u8, size:u8) -> u32 {
             let mut reg_num: u16 = code.clone();
             reg_num >>= place;
             reg_num &= 0b0000000000000111;
-            return mem.dreg[reg_num as usize] as u32;
+            reg_num *= 4;
+            return get_long(&mem.dreg, reg_num as usize)
+            // return mem.dreg[reg_num as usize] as u32;
         },
         _ => {
             process::exit(1);
         }
     }
+}
+
+fn get_word(mem:&[u8], start:usize) -> u32 {
+    if(start % 2 == 1) {panic!("Trying to address on an even address")}
+    return (mem[start + 1] as u32 *256) + mem[start] as u32;
+}
+
+fn get_long(mem:&[u8], start:usize) -> u32 {
+    if(start % 2 == 1) {panic!("Trying to address on an even address")}
+    return (mem[start + 3] as u32 *16777216)
+        + (mem[start + 2] as u32 *65536)
+        + (mem[start + 1] as u32 *256) 
+        + mem[start] as u32;
 }
 
 fn main(){
@@ -59,11 +78,12 @@ fn main(){
     // println!("This code has been executed after 3 seconds");
 
     let mut mem = Memory::default();
-    mem.dreg[3] = 87;
-    let code:u16 = 0b1101011100010001;
+    mem.dreg[4] = 255;
+    mem.dreg[5] = 87;
+    let code:u16 = 0b1101001100010001;
     // find_address_mode(code);
     // println!("{ }", 0b1101000100010001);
-    println!("{ }", get_source(mem, code, DRD, 9));
+    println!("{ }", get_source(mem, code, DRD, 9, LONG));
 }
 
 
